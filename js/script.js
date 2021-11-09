e221e5ffe65e19e0bde47062edc1adf703683c78
kadiado <marcothulio2@gmail.com>
2021-11-08 22:43:46 -0300

    js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,33 @@
+let titulo = document.querySelector('h1')
+
+titulo.addEventListener('mouseover', colorChange);
+
+function colorChange(event) {
+  titulo.style.color = 'tomato';
+}
 
 
-let img = document.createElement('img');
-img.setAttribute('src', document.querySelector('#img-url').value)
+titulo.addEventListener('mouseout', (evento) => {
+  titulo.style.color = 'black';
+})
+
+let nome = document.querySelector('#name')
+let paragraph = document.querySelector('#descricao')
+let btn = document.querySelector('#enviar')
+
+let newName = document.querySelector('h2')
+let NewParagraph = document.querySelector('p')
+
+
+btn.addEventListener('click', (Event) => {
+  console.log("clicou no trem")
+
+  Event.preventDefault();
+
+  newName.innerHTML = `${nome.value}`;
+  NewParagraph.innerHTML = `${paragraph.value}`;
+
+
+
+})
+
